cb0e1d3c3c9f02cedadde8aa1323a0c4012fcaf0
Kyle Needham <kylex691@hotmail.com>
2014-10-20 13:33:09 +0100

    Gruntfile now has tasks for building and testing

---
+++ gruntFile.js
@@ -0,0 +1,58 @@
+
+module.exports = function(grunt)
+{
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Core tasks
+  var tasks = ['coffee', 'jasmine', 'uglify'];
+
+  // Counter configuration
+  grunt.initConfig({
+    uglify: {
+      dist: {
+        files: {
+          'dist/autocomplete.min.js': 'dist/autocomplete.js'
+        }
+      }
+    },
+    coffee: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: '*.coffee',
+          dest: 'dist/',
+          ext: '.js'
+        }]
+      },
+      specs: {
+        files: [{
+          expand: true,
+          cwd: 'spec/coffeescripts/',
+          src: '*.spec.coffee',
+          dest: 'spec/javascripts/',
+          ext: '.spec.js'
+        }]
+      }
+    },
+    jasmine: {
+      src: ['dist/autocomplete.js'],
+      options: {
+        specs: 'spec/javascripts/**/*.spec.js'
+      }
+    },
+    watch: {
+      files: [
+        'src/*',
+        'spec/coffeescripts/**/*.spec.coffee'
+      ],
+      tasks: tasks
+    }
+  });
+
+  grunt.registerTask('default', tasks);
+  grunt.registerTask('travis', ['coffee', 'jasmine']);
+};
