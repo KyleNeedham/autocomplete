802801a503c5f22627f7ac0ffa97ce08dd68fd32
Kyle Needham <kylex691@hotmail.com>
2014-11-07 14:07:57 +0000

    Removed some tasks

--- gruntFile.js
+++ gruntFile.js
@@ -2,12 +2,11 @@
 module.exports = function(grunt)
 {
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Core tasks
-  var tasks = ['coffee', 'jasmine', 'uglify'];
+  var tasks = ['coffee', 'uglify'];
 
   grunt.initConfig({
     coffee: {
@@ -25,27 +24,6 @@ module.exports = function(grunt)
             'src/autocomplete.behavior.coffee'
           ]
         }
-      },
-      specs: {
-        files: [{
-          expand: true,
-          cwd: 'spec/coffeescripts/',
-          src: '*.spec.coffee',
-          dest: 'spec/javascripts/',
-          ext: '.spec.js'
-        }]
-      }
-    },
-    jasmine: {
-      src: [
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/underscore/underscore.js',
-        'node_modules/backbone/backbone.js',
-        'node_modules/backbone.marionette/lib/backbone.marionette.js',
-        'dist/autocomplete.js'
-      ],
-      options: {
-        specs: 'spec/javascripts/**/*.spec.js'
       }
     },
     uglify: {
@@ -65,5 +43,4 @@ module.exports = function(grunt)
   });
 
   grunt.registerTask('default', tasks);
-  grunt.registerTask('travis', ['coffee', 'jasmine']);
 };
