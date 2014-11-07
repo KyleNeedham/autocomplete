3a0c78775eabb78c038ba931860217af1d41aa80
Kyle Needham <kylex691@hotmail.com>
2014-11-07 14:12:11 +0000

    Removed old spec

--- spec/javascripts/autocomplete.spec.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  describe('Autocomplete', function() {
-    return it('has a fake expectation to not fail', function() {
-      return expect(true).toBe(true);
-    });
-  });
-
-}).call(this);
